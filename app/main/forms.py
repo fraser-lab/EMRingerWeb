9212d77873e54e1154c920fda569e5a86a63f85e
Benjamin Barad <benjamin.barad@gmail.com>
2014-12-27 07:09:32 -0800

    forms and models

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,10 +1,17 @@
 from flask.ext.uploads import UploadSet
 from flask.ext.wtf import Form
-from wtforms import StringField, FileField, SubmitField
+from wtforms.file import FileField, FileAllowed, FileRequired
+from wtforms import StringField, SubmitField
 from wtforms.validators import Required, Optional, Email
+model = UploadSet()
+
 
 class JobForm(Form):
-	model = FileField('PDB Model', validators=[Required()])
-	map = FileField('CCP4 Map', validators=[Required()])
+	model = FileField('PDB Model', validators=[FileAllowed(pdb_type, "EMRinger doesn't recognize these files. Try using a pdb or ent file", FileRequired()])
+	map = FileField('CCP4 Map', validators=[FileRequired()])
 	email = StringField('Email for Results Link', validators=[Optional(), Email()])
-	
+	submit = SubmitField('Send Job')
+
+class EmailForm(Form):
+	email = FileField('Email for Results Link', validators=[Required()])
+	submit = SubmitField('Send Job')
---
+++ app/models.py
@@ -0,0 +1,42 @@
+from . import db
+
+class Job(db.model):
+	__tablename__ = 'jobs'
+	id = db.Column(db.Integer, primary_key=True)
+	uid = db.Column(db.String, unique=True, index=True)
+	datetime = db.Column(db.DateTime)
+	email = db.Column(db.String, nullable=True, index = True, unique = True)
+	map = db.Column(db.String)
+	model = db.Column(db.String)
+	success = db.Column(db.Boolean)
+	residues = db.relationship('Residue', backref="job")
+
+	def __repr__(self):
+		if self.email:
+			return "Job by %s, submitted at %s" % (self.email, self.datetime)
+		else:
+			return "Job %s without email, submitted at %s" % (self.uid, self.datetime)
+
+class Residue(db.model):
+	__tablename__= 'residues'
+	id = db.Column(db.Integer, primary_key=True)
+	res_type = db.Column(db.String, index=True)
+	res_id = db.Column(db.String, index=True)
+	chain_id = db.Column(db.String, index=True)
+	peak_angle = db.Column(db.Integer, index=True)
+	peak_density = db.Column(db.Float, index=True)
+	angles = db.relationship('Angle', backref = 'residue')
+	job_id = db.Column(db.Integer, db.ForeignKey('jobs.id'), index=True)
+
+	def __repr__(self):
+		return "Residue %s %d of chain %s" % (self.res_type, self.res_id, self.chain_id)
+
+class Angle(db.model):
+	__tablename__='angles'
+	id = db.Column(db.Integer, primary_key=True)
+	angle = db.Column(db.Integer)
+	density = db.Column(db.Integer)
+	residue_id = db.Column(db.Integer, db.)ForeignKey('residues.id'), index=True)
+	
+	def __repr__(self):
+		return "At angle %s, the electron potential density is %s" % (self.angle, self.density)
