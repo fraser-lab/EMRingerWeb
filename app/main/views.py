f397f3c565a3e8f0e19cd67bbd26aac60ff9c946
Benjamin Barad <benjamin.barad@gmail.com>
2015-01-02 12:09:37 -0800

    more work on flask file uploads, and a rudimentary humans.txt

--- app/main/views.py
+++ app/main/views.py
@@ -6,6 +6,24 @@ from .. import db
 from ..models import Job, Residue, Angle
 from werkzeug import secure_filename
 
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def gen_file_name(filename):
+    """
+    If file was exist already, rename it and return a new name
+    """
+
+    i = 1
+    while os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], filename)):
+        name, extension = os.path.splitext(filename)
+        filename = '%s_%s%s' % (name, str(i), extension)
+        i = i + 1
+
+    return filename
+
 @main.route('/', methods= ['GET', 'POST'])
 # """The homepage for the app"""
 def index():
--- config.py
+++ config.py
@@ -7,7 +7,9 @@ class Config:
 	EMRINGER_MAIL_SUBJECT_PREFIX = '[EMRinger]'
 	EMRINGER_MAIL_SENDER = 'EMRinger Server <donotreply@emringer.com>'
 	EMRINGER_ADMIN = os.environ.get('EMRINGER_ADMIN')
-
+	ALLOWED_EXTENSIONS={'MAP':set(['ccp4','map','mrc']),'PDB':set(['pdb','ent'])}
+	IGNORED_FILES = set(['.gitignore'])
+	
 	@staticmethod
 	def init_app(app):
 		pass
@@ -19,17 +21,26 @@ class DevelopmentConfig(Config):
 	MAIL_USE_TLS = True
 	MAIL_USERNAME = os.environ.get('GMAIL_USER')
 	MAIL_PASSWORD = os.environ.get('GMAIL_PASSWORD')
+	UPLOAD_FOLDER = os.path.join(basedir,'data_dev/')
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
 		'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+	MAX_CONTENT_LENGTH = 2000 * 1024 * 1024 
+
 
 class TestingConfig(Config):
  	TESTING = True
+ 	UPLOAD_FOLDER = os.path.join(basedir,'data_test/')
  	SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
  		'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+	MAX_CONTENT_LENGTH = 200 * 1024 * 1024
+
 
 class ProductionConfig(Config):
+	UPLOAD_FOLDER = os.environ.get('EMRINGER_UPLOAD_FOLDER') or os.path.join(basedir,'data/')
  	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
  		'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+ 	MAX_CONTENT_LENGTH = 200 * 1024 * 1024
+
 
 config = {
 	'development': DevelopmentConfig,
