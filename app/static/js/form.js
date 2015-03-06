9b927bbb478620ffd46296a9639da750c2ab58d9
bbarad <benjamin.barad@gmail.com>
2015-03-06 13:29:45 -0800

    set up frontend

---
+++ app/static/js/form.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+  console.log("Model Uploader Test");
+  var modelUploader = $('#modelUploader').fineUploader({
+    request: {
+      endpoint: 'server/success.html'
+    },
+    template: 'qq-template-model',
+    classes: {
+      success: 'alert alert-success',
+      fail: 'alert alert-error'
+    },
+    multiple: false,
+    validation: {
+      allowedExtensions: ['pdb','ent'],
+      sizeLimit: 20000000 // 20 MB = 100 * 1024 * 1024 bytes
+    },
+    autoUpload: false,
+    showMessage: function(message) {
+      // Using Bootstrap's classes
+      $('#modelError').remove()
+      $('#modelUploader').append('<div id="modelError" class="alert alert-danger"><span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span> ' + message + '</div>');
+    }
+  });
+
+  console.log("Map Uploader Test");
+  var mapUploader = $('#mapUploader').fineUploader({
+    request: {
+      endpoint: 'server/success.html'
+    },
+    template: 'qq-template-map',
+    multiple: false,
+    validation: {
+      allowedExtensions: ['map','mrc','ccp4'],
+      sizeLimit: 100000000 // 100 MB = 100 * 1024 * 1024 bytes
+    },
+    autoUpload: false,
+    showMessage: function(message) {
+      // Using Bootstrap's classes
+      $('#mapError').remove()
+      $('#mapUploader').append('<div id="mapError" class="alert alert-danger"><span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span> ' + message + '</div>');
+    }
+  });
+
+  console.log("Map Uploader Test");
+  $('#triggerUpload').click(function() {
+    modelUploader.fineUploader('uploadStoredFiles');
+    mapUploader.fineUploader('uploadStoredFiles');
+  });
+});
