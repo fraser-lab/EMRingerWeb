7505f92e701420a9fcdbd43d6695b1f924dc5051
bbarad <benjamin.barad@gmail.com>
2015-09-15 08:32:14 -0700

    410 error page for expired data

--- emringerweb/main/errors.py
+++ emringerweb/main/errors.py
@@ -7,6 +7,10 @@ from emringerweb.main import main_blueprint
 def page_not_found(e):
 	return render_template('404.html'), 404
 
+@main_blueprint.app_errorhandler(410)
+def page_not_found(e):
+  return render_template('410.html'), 410
+
 @main_blueprint.app_errorhandler(500)
 def internal_error(e):
 	return render_template('500.html'), 500
